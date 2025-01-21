3488c2433ef408c1437f0754881ed7b9e04d41fb
alexrod94 <alexrod94@gmail.com>
2025-01-21 18:40:21 +0100

    Change home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* Home page of a website */}
+      <h1>Home</h1>
+      <p>This is the home page of a website.</p>
+      <button>Click me!</button>
+    </div>
+  );
 }
 
 export default Home;
